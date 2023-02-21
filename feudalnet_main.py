4bdbdb2bc3d79fea5636b524a20e3314313b6201
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-02-21 23:33:55 +0900

    float sibal

--- feudalnet_main.py
+++ feudalnet_main.py
@@ -41,13 +41,13 @@ parser.add_argument('--hidden-dim-worker', type=int, default=32,
 #                     help="discount factor worker")
 # parser.add_argument('--gamma-m', type=float, default=0.99,
 #                     help="discount factor manager")
-parser.add_argument('--gamma-w', type=float, default=1,
+parser.add_argument('--gamma-w', type=float, default=0.9,
                     help="discount factor worker")
-parser.add_argument('--gamma-m', type=float, default=1,
+parser.add_argument('--gamma-m', type=float, default=0.95,
                     help="discount factor manager")
 parser.add_argument('--alpha', type=float, default=0.5,
                     help='Intrinsic reward coefficient in [0, 1]')
-parser.add_argument('--eps', type=float, default=int(1e-4),
+parser.add_argument('--eps', type=float, default=float(1e-2),
                     help='Random Gausian goal for exploration')
 parser.add_argument('--dilation', type=int, default=10,
                     help='Dilation parameter for manager LSTM.')
