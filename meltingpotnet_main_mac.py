ff7d469ad1fdf63896fc996fde7cb8a613d12151
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-03-22 20:18:36 +0900

    update melting

--- meltingpotnet_main_mac.py
+++ meltingpotnet_main_mac.py
@@ -14,7 +14,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description='MPnet')
 # GENERIC RL/MODEL PARAMETERS
-parser.add_argument('--lr', type=float, default=1e-3,
+parser.add_argument('--lr', type=float, default=1e-5,
                     help='learning rate')
 parser.add_argument('--env-name', type=str, default='MiniGrid-FourRooms-v0',   #'MiniGrid-FourRooms-v0' 'MiniGrid-DoorKey-5x5-v0' 'MiniGrid-Empty-16x16-v0'
                     help='gym environment name')
@@ -24,7 +24,7 @@ parser.add_argument('--num-workers', type=int, default=16,
 #                     help='number of steps the agent takes before updating')
 parser.add_argument('--num-steps', type=int, default=100,
                     help='number of steps the agent takes before updating')
-parser.add_argument('--max-steps', type=int, default=int(1e6),
+parser.add_argument('--max-steps', type=int, default=int(1e7),
                     help='maximum number of training steps in total')
 parser.add_argument('--cuda', type=bool, default=True,
                     help='Add cuda')
